4a7d6e196fdbff310d466ee49990de8844890789
rimtest1 <rimtest160805@gmail.com>
2024-09-12 09:10:33 +0500

    Add files via upload
    Первое задание сделано.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class Program
+{
+    static void Main()
+    {
+        int a = 5, b = 3, c = 10, d = -2;
+        int minValue = Min(Min(a, b), Min(c, d));
+        Console.WriteLine(minValue);
+        Console.ReadLine();
+    }
+
+    static int Min(int x, int y)
+    {
+        return (x < y) ? x : y;
+    }
+}
